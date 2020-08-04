4691937ea9331321c6165ca8dc2f1c57a829c36c
Stefi Gandhi <smasal00@g.uafs.edu>
2020-08-04 18:43:31 -0500

    Updates

---
+++ precache-manifest.04170d98243d888bda7eaa2d11186665.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd199ad456dbf502afb8ce240549e5b",
+    "url": "/sgclass10webFrame/index.html"
+  },
+  {
+    "revision": "ee138fa0210246e575a1",
+    "url": "/sgclass10webFrame/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f116455603edbe0992a",
+    "url": "/sgclass10webFrame/static/js/2.62140daf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sgclass10webFrame/static/js/2.62140daf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee138fa0210246e575a1",
+    "url": "/sgclass10webFrame/static/js/main.e12d0de6.chunk.js"
+  },
+  {
+    "revision": "02287cf66e75450a41e6",
+    "url": "/sgclass10webFrame/static/js/runtime-main.6961b495.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sgclass10webFrame/static/media/logo.5d5d9eef.svg"
+  }
+]);
